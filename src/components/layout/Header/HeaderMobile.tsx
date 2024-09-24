23184bcffff1dd5b206c3794d7780f7877244a69
esis8 <eesis.dev@gmail.com>
2024-09-24 13:38:17 -0300

    comment google Connect

--- src/components/layout/Header/HeaderMobile.tsx
+++ src/components/layout/Header/HeaderMobile.tsx
@@ -2,19 +2,19 @@ import styles from "./HeaderMobile.module.scss";
 import BtnCampaign from "../../buttons/campaign/BtnCampaign";
 import BtnConnectWallet from "../../buttons/connectWallet/BtnConnectWallet";
 import NavMenu from "./NavMenu";
-import { Session } from "next-auth";
-import GoogleConnect from "@/components/buttons/googleConnect/GoogleConnect";
+//import { Session } from "next-auth";
+//import GoogleConnect from "@/components/buttons/googleConnect/GoogleConnect";
 
-export default function HeaderMobile({ session }: { session: Session | null }) {
+export default function HeaderMobile(/* { session }: { session: Session | null } */) {
   return (
     <div className={styles.headerMobile}>
       <BtnCampaign type="mobile" />
-      {session === null ? (
+      {/* {session === null ? (
         <BtnConnectWallet type="mobile" />
       ) : (
         <GoogleConnect loggedIn={true} />
-      )}
-
+      )} */}
+      <BtnConnectWallet type="mobile" />
       <NavMenu />
     </div>
   );
