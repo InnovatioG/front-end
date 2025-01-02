e6d9629045c5a3797e7196eca1685be48cac7d67
manupadillaph <manuepad@gmail.com>
2025-01-02 13:58:03 -0300

    added commons to smart db lib, with constans, types for params and redeemers moved here

---
+++ src/lib/SmartDB/Commons/Params/index.ts
@@ -0,0 +1,71 @@
+import { Script } from 'lucid-cardano';
+import { scriptSchema, yup } from 'smart-db/backEnd';
+
+export interface ProtocolCreateParams {
+    name: string;
+    configJson: string;
+}
+
+// export interface SellMarketNFTTxParams {
+//     token_TN: string;
+//     token_CS: string;
+//     datumID_CS: string;
+//     datumID_TN: string;
+//     validatorAddress: string;
+//     mintingPolicyID: Script;
+//     validatorMarket: Script;
+//     priceOfAsset: bigint;
+// }
+// export interface BuyMarketNFTTxParams {
+//     token_TN: string;
+//     token_CS: string;
+//     datumID_CS: string;
+//     datumID_TN: string;
+//     marketNft_id: string;
+//     sellerAddress: string;
+//     mintingPolicyID: Script;
+//     validatorMarket: Script;
+//     priceOfAsset: bigint;
+// }
+
+// export interface WithdrawMarketNFTTxParams {
+//     token_TN: string;
+//     token_CS: string;
+//     datumID_CS: string;
+//     datumID_TN: string;
+//     marketNft_id: string;
+//     mintingPolicyID: Script;
+//     validatorMarket: Script;
+// }
+// export const sellMarketNFTTxParamsSchema = yup.object().shape({
+//     token_TN: yup.string().required(),
+//     token_CS: yup.string().required(),
+//     datumID_CS: yup.string().required(),
+//     datumID_TN: yup.string().required(),
+//     validatorAddress: yup.string().required(),
+//     mintingPolicyID: scriptSchema.required(),
+//     validatorMarket: scriptSchema.required(),
+//     priceOfAsset: yup.number().required(),
+// });
+
+// export const buyMarketNFTTxParamsSchema = yup.object().shape({
+//     token_TN: yup.string().required(),
+//     token_CS: yup.string().required(),
+//     datumID_CS: yup.string().required(),
+//     datumID_TN: yup.string().required(),
+//     marketNft_id:  yup.string().required(),
+//     sellerpayment_pkh:  yup.string().required(),
+//     mintingPolicyID: scriptSchema.required(),
+//     validatorMarket: scriptSchema.required(),
+//     priceOfAsset: yup.number().required(),
+// });
+
+// export const withdrawMarketNFTTxParamsSchema = yup.object().shape({
+//     token_TN: yup.string().required(),
+//     token_CS: yup.string().required(),
+//     datumID_CS: yup.string().required(),
+//     datumID_TN: yup.string().required(),
+//     marketNft_id:  yup.string().required(),
+//     mintingPolicyID: scriptSchema.required(),
+//     validatorMarket: scriptSchema.required(),
+// });
