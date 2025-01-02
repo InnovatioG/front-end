7bc47b79c3f69fc8c4fe9a2becac98c11a5fb188
manupadillaph <manuepad@gmail.com>
2025-01-02 13:56:46 -0300

    added callback after load example

--- src/lib/SmartDB/BackEnd/Campaign.BackEnd.Api.Handlers.ts
+++ src/lib/SmartDB/BackEnd/Campaign.BackEnd.Api.Handlers.ts
@@ -7,6 +7,7 @@ import {
     BaseSmartDBBackEndApiHandlers,
     BaseSmartDBBackEndApplied,
     BaseSmartDBBackEndMethods,
+    CascadeUpdate,
     NextApiRequestAuthenticated,
     console_error,
     console_log
@@ -17,6 +18,128 @@ import { CampaignEntity } from '../Entities/Campaign.Entity';
 export class CampaignBackEndApplied extends BaseSmartDBBackEndApplied {
     protected static _Entity = CampaignEntity;
     protected static _BackEndMethods = BaseSmartDBBackEndMethods;
+
+    // #region callbacks
+
+    public static async callbackOnAfterLoad<T extends BaseEntity>(instance: T, cascadeUpdate: CascadeUpdate): Promise<CascadeUpdate> {
+        //--------------------------------------
+        console_log(1, this._Entity.className(), `callbackOnAfterLoad - Init`);
+        //--------------------------------------
+        cascadeUpdate = await super.callbackOnAfterLoad(instance, cascadeUpdate);
+        //--------------------------------------
+        // if (cascadeUpdate.swUpdate) {
+        //     console_log(0, instance.className(), `callbackOnAfterLoad - updating because super.callbackOnAfterLoad...`);
+        // }
+        // //--------------------------------------
+        // const fundInstance = instance as unknown as FundWithDetailsEntity;
+        // //-------------------
+        // if (fundInstance._isDeployed === true) {
+        //     //-------------------
+        //     const total_FT_Minted = await this.getTotal_FT_Minted(fundInstance);
+        //     //--------------------------------------
+        //     let priceADAx1e6 = fundInstance.priceADAx1e6;
+        //     //--------------------------------------
+        //     let TVLx1e6 = fundInstance.TVLx1e6;
+        //     //--------------------------------------
+        //     const delegatedMAYZ = await this.getTotal_MAYZ_Delegated(fundInstance);
+        //     //--------------------------------------
+        //     const feeEarned = await this.getTotal_FT_Commissions_Earned(fundInstance);
+        //     //--------------------------------------
+        //     let tokenFTWithPrice: Token_With_Price_And_Date_And_Signature | undefined = undefined;
+        //     //--------------------------------------
+        //     if (fundInstance.investUnit?.investUnitWithADAPrices.iuValuesWithDetailsAndAmount !== undefined) {
+        //         tokenFTWithPrice = await this.get_TokenFT_PriceADAx1e6_From_TokensWithDetails(fundInstance, false, MAX_PRICE_AGE_FOR_APROXIMATED_USE_MS);
+        //     } else {
+        //         tokenFTWithPrice = await this.get_TokenFT_PriceADAx1e6(fundInstance, false, MAX_PRICE_AGE_FOR_APROXIMATED_USE_MS);
+        //     }
+        //     //--------------------------------------
+        //     if (tokenFTWithPrice !== undefined && tokenFTWithPrice.priceADAx1e6 !== undefined) {
+        //         priceADAx1e6 = tokenFTWithPrice.priceADAx1e6;
+        //         TVLx1e6 = tokenFTWithPrice.priceADAx1e6 * total_FT_Minted;
+        //     }
+        //     //------------------------------
+        //     const change24 = await this.get_FT_Change_24(fundInstance);
+        //     //------------------------------
+        //     const prev = {
+        //         total_FT_Minted: fundInstance.total_FT_Minted,
+        //         priceADAx1e6: fundInstance.priceADAx1e6,
+        //         TVLx1e6: fundInstance.TVLx1e6,
+        //         change24: fundInstance.change24,
+        //         feeEarned: fundInstance.feeEarned,
+        //         delegatedMAYZ: fundInstance.delegatedMAYZ,
+        //     };
+        //     //------------------------------
+        //     fundInstance.total_FT_Minted = total_FT_Minted;
+        //     fundInstance.priceADAx1e6 = priceADAx1e6;
+        //     fundInstance.TVLx1e6 = TVLx1e6;
+        //     fundInstance.change24 = change24;
+        //     fundInstance.feeEarned = feeEarned;
+        //     fundInstance.delegatedMAYZ = delegatedMAYZ;
+        //     //------------------------------
+        //     const current = {
+        //         total_FT_Minted: fundInstance.total_FT_Minted,
+        //         priceADAx1e6: fundInstance.priceADAx1e6,
+        //         TVLx1e6: fundInstance.TVLx1e6,
+        //         change24: fundInstance.change24,
+        //         feeEarned: fundInstance.feeEarned,
+        //         delegatedMAYZ: fundInstance.delegatedMAYZ,
+        //     };
+        //     //------------------------------
+        //     let updatedFields = {};
+        //     let swUpdateValues = false;
+        //     //------------------------------
+        //     Object.entries(prev).forEach(([key, value]) => {
+        //         if ((current as any)[key] !== value) {
+        //             (updatedFields as any)[key] = {
+        //                 from: value,
+        //                 to: (current as any)[key],
+        //             };
+        //             swUpdateValues = true;
+        //         }
+        //     });
+        //     //------------------------------
+        //     if (swUpdateValues) {
+        //         console_log(0, instance.className(), `callbackOnAfterLoad - updating because updatedValues - which fields: ${toJson({ updatedFields })}`);
+        //         cascadeUpdate = { swUpdate: true, updatedFields };
+        //     }
+        //     //------------------------------
+        //     const CS = fundInstance.getNET_id_CS();
+        //     const TN_Hex = fundInstance.fdFundFT_TN;
+        //     //--------------------------------------
+        //     const TokenMetadataBackEndApplied = (await import('../index.exports')).TokenMetadataBackEndApplied;
+        //     //--------------------------------------
+        //     const tokenMetadata: Record<string, any> | undefined = await TokenMetadataBackEndApplied.get_Token_Metadata(
+        //         CS,
+        //         TN_Hex,
+        //         undefined,
+        //         TokenMetadataEntity.optionsGetForTokenStore
+        //     );
+        //     //--------------------------------------
+        //     const tokenFTWithPriceAndMetadataAndAmount: Partial<Token_With_Price_And_Date_And_Signature_And_Amount_And_Metadata> = {
+        //         ...tokenFTWithPrice,
+        //         CS,
+        //         TN_Hex,
+        //         amount: total_FT_Minted,
+        //         decimals: tokenMetadata?.decimals,
+        //         image: tokenMetadata?.image,
+        //         colorHex: tokenMetadata?.colorHex,
+        //         metadata_raw: tokenMetadata?.metadata,
+        //     };
+        //     //--------------------------------------
+        //     fundInstance.tokenFT = tokenFTWithPriceAndMetadataAndAmount as unknown as Token_With_Price_And_Date_And_Signature_And_Amount_And_Metadata;
+        //     //--------------------------------------
+        // } else {
+        //     //--------------------------------------
+        //     fundInstance.tokenFT = undefined;
+        //     //--------------------------------------
+        // }
+        //--------------------------------------
+        console_log(-1, this._Entity.className(), `callbackOnAfterLoad  - OK`);
+        //-------------------------------------
+        return cascadeUpdate;
+    }
+
+    // #endregion callbacks
 }
 
 @BackEndApiHandlersFor(CampaignEntity)
