3cc2f80b459533d2e9a775e5cf4c3bc9b7c3b6d3
Manuel Padilla <manuelpad@gmail.com>
2025-06-03 13:12:06 -0300

    Enhance S3 upload logging and refactor AWS config into separate object

--- src/utils/s3Upload.ts
+++ src/utils/s3Upload.ts
@@ -1,11 +1,15 @@
 import AWS from 'aws-sdk';
+import { toJson } from 'smart-db';
 
 // Configura el SDK de AWS con tus credenciales y la región de tu bucket S3
-AWS.config.update({
+
+const paramsConnectionS3 = {
     accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
     region: process.env.NEXT_PUBLIC_AWS_REGION,
-});
+};
+
+AWS.config.update(paramsConnectionS3);
 
 const s3 = new AWS.S3();
 
@@ -19,7 +23,7 @@ export const uploadFileToS3 = async (file: File, bucketName: string, key: string
     };
 
     try {
-        console.log(`File uploading to S3: ${key}`);
+        console.log(`File uploading to S3: ${key} - Params: ${toJson(params)} - Params Connection: ${toJson(paramsConnectionS3)}`);
         const data = await s3.upload(params).promise();
         console.log(`File uploaded to S3: ${key}`);
         return data.Location;
@@ -48,11 +52,11 @@ export const uploadBlobURLToS3 = async (blobUrl: string): Promise<string> => {
 
         // Get the file extension from the MIME type
         const mimeType = blob.type; // Example: "image/jpeg"
-        const fileExtension = mimeType.split("/")[1]; // Extracts "jpeg", "png", etc.
+        const fileExtension = mimeType.split('/')[1]; // Extracts "jpeg", "png", etc.
 
         // Ensure a valid file extension (fallback to 'png' if unknown)
-        const validExtensions = ["png", "jpeg", "jpg", "webp", "gif"];
-        const extension = validExtensions.includes(fileExtension) ? fileExtension : "png";
+        const validExtensions = ['png', 'jpeg', 'jpg', 'webp', 'gif'];
+        const extension = validExtensions.includes(fileExtension) ? fileExtension : 'png';
 
         // Create a File object with the correct extension
         const file = new File([blob], `avatar-${Date.now()}.${extension}`, { type: mimeType });
@@ -62,7 +66,7 @@ export const uploadBlobURLToS3 = async (blobUrl: string): Promise<string> => {
 
         return await uploadFileToS3(file, bucketName, key);
     } catch (error) {
-        console.error("Error converting Blob to File for S3 upload:", error);
+        console.error('Error converting Blob to File for S3 upload:', error);
         throw error;
     }
 };
